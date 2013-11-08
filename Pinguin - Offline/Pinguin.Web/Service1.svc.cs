31786260b18e25bbec2593a2addca00911138bfd
andyVerbaenen <andy.verbaenen@student.ap.be>
2013-11-08 09:25:54 +0100

    1ste commit

---
+++ Pinguin - Offline/Pinguin.Web/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Web.UI.WebControls;
+
+namespace Pinguin.Web
+{
+    [ServiceContract(Namespace = "")]
+    [SilverlightFaultBehavior]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1
+    {
+        [OperationContract]
+        public int RolDobbelsteen()
+        {
+            
+            return 4;
+        }
+        //Random random = new Random();
+        //[OperationContract]
+        //public int[,] MakeMap()
+        //{
+        //    int[,] map = new int[10, 10];
+        //    for (int i = 0; i < 10; i++)
+        //    {
+
+        //        for (int j = 0; j < 10; j++)
+        //        {
+        //            map[i, j] = new int();
+        //            map[i, j] = random.Next(1, 4);
+        //        }
+        //    }
+        //    return map;
+        //}
+        
+    }
+}
