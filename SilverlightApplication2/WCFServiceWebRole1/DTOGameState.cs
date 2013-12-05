813a523357c8ad1ef04b8e0c45042cf895184118
JonathanSSS <jonathan.sansens@student.artesis.be>
2013-12-05 20:00:51 +0100

    Werking spel compleet!

---
+++ SilverlightApplication2/WCFServiceWebRole1/DTOGameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceWebRole1
+{
+    public class DTOGameState
+    {
+        public List<DTOIjsschots> AllIjsschots { get; set; }
+        public int HetIsAan { get; set; }
+        public string KleurSpeler { get; set; }
+        public List<DTOPion> AllPion { get; set; }
+        public List<DTOSpeler> AllSpeler { get; set; }
+    }
+}
